3a1afb6737865766f49947fd6841d2f5e6d83c90
Zach Cohn <cohn.zach@gmail.com>
2023-05-14 08:03:15 +0000

    Change fallback port due to conflict

--- server/server.js
+++ server/server.js
@@ -36,5 +36,5 @@ app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/logout', logoutRouter);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
