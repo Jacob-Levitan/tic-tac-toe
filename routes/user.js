02a926cbdb81035c7537f8cf19c955c7e759df1a
Zach Cohn <cohn.zach@gmail.com>
2023-06-11 09:14:02 +0000

    Fixes to user update implementation and alignment with front end

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { authenticateToken } = require('../server/jwt');
 const User = require('../models/user');
-
 const router = express.Router();
+const USERNAME_REGEX = /^[A-z][A-z0-9-_]{3,20}$/;
+const NON_NEG_NUMBER_REGEX = /^\d+$/;
+const MY_ERRORS = ["bad role", "username unavailable", "bad username", "bad number"]
 
 router.get('/', authenticateToken, (req, res) => {
     const user = req.user;
@@ -16,20 +18,45 @@ async function updateUsername(req, res) {
     try {
         const existingUsername = await User.getUserByUsername(req.body.username);
 
-        if (existingUsername) return res.sendStatus(403);
+        if (existingUsername) {
+            res.status(403).json({ message: "Username not available" });
+            throw "username unavailable"
+        }
+
+        if (!USERNAME_REGEX.test(req.body.username)) {
+            res.status(400).json({ message: "Invalid username" });
+            throw "bad username"
+        }
 
         req.user.username = req.body.username;
     } catch (err) {
-        console.log(err);
-        return res.sendStatus(500);
+        throw err;
     }
 }
 
 function updateWins(req, res) {
+    try {
+        if (!NON_NEG_NUMBER_REGEX.test(req.body.wins)) {
+            res.status(400).json({ message: "Wins must be non-negative whole number" });
+            throw "bad number";
+        }
+    } catch (err) {
+        throw err;
+    }
+
     req.user.wins = req.body.wins;
 }
 
 function updateLosses(req, res) {
+    try {
+        if (!NON_NEG_NUMBER_REGEX.test(req.body.losses)) {
+            res.status(400).json({ message: "Losses must be non-negative whole number" });
+            throw "bad number";
+        }
+    } catch (err) {
+        throw err;
+    }
+
     req.user.losses = req.body.losses;
 }
 
@@ -37,31 +64,41 @@ async function updateUser(req, res) {
     try {
         await req.user.save();
     } catch (err) {
-        console.log(err);
-        return res.sendStatus(500);
+        throw err;
     }
 }
 
 function checkRole(req, res) {
-    if ((req?.body?.wins || req?.body?.losses) && !req.user.roles.includes('editor'))
-        return res.sendStatus(401);
+    if ((req?.body?.wins || req?.body?.losses) && !req.user.roles.includes('editor')) {
+        res.status(403).json({ message: "Must have editor role" });
+        throw "bad role";
+    }
 }
 
 router.put('/', authenticateToken, async (req, res) => {
-    checkRole(req, res);
 
-    if (req?.body?.username)
-        await updateUsername(req, res);
+    try {
+        checkRole(req, res);
 
-    if (req?.body?.wins)
-        updateWins(req, res);
+        if (req?.body?.username)
+            await updateUsername(req, res);
 
-    if (req?.body?.losses)
-        updateLosses(req, res);
+        if (req?.body?.wins)
+            updateWins(req, res);
 
-    await updateUser(req, res);
+        if (req?.body?.losses)
+            updateLosses(req, res);
 
-    return res.sendStatus(200);
+        await updateUser(req, res);
+
+        return res.sendStatus(200);
+
+    } catch (err) {
+        if (!MY_ERRORS.includes(err)) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    }
 });
 
 module.exports = router;
